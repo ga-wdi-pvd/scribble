14231a30047e106cd42c82a028d4a5a0002d1112
patrickthesailorman <patrickthesaiorman@gmail.com>
2016-12-19 10:42:53 -0500

    better

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,25 @@
 class CommentsController < ApplicationController
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+
   def create
       @post = Post.find(params[:post_id])
       @comment = @post.comments.create(comment_params)
       redirect_to post_path(@post)
+  end
+
+  def edit
+  @post = Post.find(params[:post_id])
+  @comment = Comment.find(params[:id])
+  end
+
+  def update
+  @post = Post.find(params[:post_id])
+  @comment = Comment.find(params[:id])
+  @comment.update(comment_params)
+  redirect_to post_path(@post)
   end
 
   def destroy
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161218211702) do
+ActiveRecord::Schema.define(version: 20161219134551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20161218211702) do
     t.string   "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id", using: :btree
   end
 
   create_table "posts", force: :cascade do |t|
@@ -29,6 +31,8 @@ ActiveRecord::Schema.define(version: 20161218211702) do
     t.datetime "updated_at", null: false
     t.string   "completed"
     t.string   "title"
+    t.string   "text"
   end
 
+  add_foreign_key "comments", "posts"
 end
