78b5013389a0e93efffeb0384c3f24577df2ac24
mconnors333 <matthew.connors333@gmail.com>
2016-12-18 22:19:53 -0500

    Connors Week05 Day05 HW

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,32 @@
+class CommentsController < ApplicationController
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params.merge(user: current_user))
+    redirect_to post_path(@post)
+  end
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params.merge(user:current_user))
+    redirect_to post_path(@post)
+  end
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+  private
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
+Contact GitHub API Training Shop Blog About
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+  def show
+    @post = Post.find(params[:id])
+  end
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.create!(post_params.merge(user: current_user))
+    redirect_to post_path(@post)
+  end
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params.merge(user:current_user))
+    redirect_to post_path(@post)
+  end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
