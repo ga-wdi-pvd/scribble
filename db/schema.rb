b98e149375f4db77c6879ce12db78e72a08fc7d0
awanderlyss <alyssa.felix7@gmail.com>
2016-12-18 22:59:24 -0500

    Update tag table, then migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161219032534) do
+ActiveRecord::Schema.define(version: 20161219035636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,5 +37,17 @@ ActiveRecord::Schema.define(version: 20161219032534) do
     t.text     "body"
   end
 
+  create_table "tags", force: :cascade do |t|
+    t.string   "description"
+    t.integer  "post_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["category_id"], name: "index_tags_on_category_id", using: :btree
+    t.index ["post_id"], name: "index_tags_on_post_id", using: :btree
+  end
+
   add_foreign_key "comments", "posts"
+  add_foreign_key "tags", "categories"
+  add_foreign_key "tags", "posts"
 end
