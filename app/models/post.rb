697640be06509cc18bed86bc0166b13fa334475c
awanderlyss <alyssa.felix7@gmail.com>
2016-12-18 21:01:50 -0500

    Fix BUG#2, comment now only if true

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
-  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 4 }
+  validates :body,  :presence => true
+  
+  has_many :comments, dependent: :destroy
 end
