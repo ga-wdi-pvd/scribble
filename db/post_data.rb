2d946689ed29873013e5302622379ae4f134481a
erikslewis <Adelina198527@gmail.com>
2016-12-19 08:30:40 -0500

    homework

---
+++ db/post_data.rb
@@ -0,0 +1,30 @@
+def get_post_data
+  post_data = {
+    "Hello1" => {
+                    name: "Hello",
+                    title: "Greet",
+                    body: "inforamtion"
+                  },
+    "Hello2" => {
+                    name: "Dog",
+                    title: "Dog House",
+                    body: "where is the dog house"
+                  },
+    "Hello3" => {
+                    name: "Cat",
+                    title: "Cats are us",
+                    body: "do you like cats"
+                  },
+    "Hello4" => {
+                    name: "WDI",
+                    title: "Providence WDI",
+                    body: "are you a student of WDI"
+                  },
+    "Hello5" => {
+                    name: "Erik",
+                    title: "The best",
+                    body: "Erik is the best right now"
+                  }
+    }
+  return post_data
+end
