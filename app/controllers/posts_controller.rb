603eb7465c49d410b25f30fe85846699e71f0ef2
Jesse Ellis <jesseellis@Jesses-MBP.fios-router.home>
2016-12-18 21:43:14 -0500

    Implement CRUD for Post and Comment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,36 @@
 class PostsController < ApplicationController
+  before_action :set_post, except: [:index, :new, :create]
+
   def index
     @posts = Post.all
   end
 
-  def show
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to @post
+  end
+
+  def update
+    @post.update(post_params)
+    redirect_to @post
+  end
+
+  def destroy
+    @post.delete
+    redirect_to posts_path
+  end
+
+  private
+
+  def set_post
     @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
