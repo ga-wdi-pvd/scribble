0f8817faee5286536a1beb4209ee40b8efbcbccc
myztajay <myztajay@hotmail.com>
2016-12-18 20:41:53 -0500

    Added form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'posts#index'
+  get '/post/:id', to: 'posts#show'
+
+  resources :posts, only: [:show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
